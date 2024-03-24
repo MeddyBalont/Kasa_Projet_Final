d28ac657cddcdbd7013b0c48a9d95fb55f02ae80
MeddyBalont <Meddy.Balont@hotmail.com>
2024-03-24 12:30:03 +0100

    Nouvelle architecture

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header_logo">
+                <img src={logo} alt="Logo Kasa" className="header_logo_img" />
+            </div>
+        </header>
+    )
+}
--- src/pages/Home/index.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return(
-        <HomeWrapper>
-            Meddy
-        </HomeWrapper>
-    )
-}
-
-export default Home
